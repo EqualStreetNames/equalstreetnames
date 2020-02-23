e7702ac86f70ef816869bfc9d7a9cc50aee1ff15
Jonathan Beliën <dev@jbelien.be>
2020-02-23 19:17:40 +0100

    Split street listing process from main process

--- scripts/overpass/associatedStreet.php
+++ scripts/overpass/associatedStreet.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-chdir(__DIR__.'/../../');
+chdir(__DIR__ . '/../../');
 
 require 'vendor/autoload.php';
 
@@ -18,21 +18,7 @@ if (!file_exists($directory) || !is_dir($directory)) {
     mkdir($directory);
 }
 
-// Get all the associatedStreet relations in all the municipalities in Brussels Region.
-foreach ($municipalities as $nis5 => $municipality) {
-    printf('%d - %s%s', $nis5, $municipality[0], PHP_EOL);
-
-    file_put_contents(
-        sprintf('%s/%s.csv', $directory, strtolower($municipality[0])),
-        get($nis5)
-    );
-
-    sleep(mt_rand(30, 120));
-}
-
 // Get all the associatedStreet relations in Brussels Region.
-printf('FULL%s', PHP_EOL);
-
 file_put_contents(
     sprintf('%s/full.json', $directory),
     get()
@@ -43,18 +29,11 @@ exit(0);
 /**
  * Run Overpass API.
  *
- * @param int|null $nis5 NIS code of the municipality (or null to have the whole Brussels Region).
- *
- * @return string CSV or JSON response from Overpass.
+ * @return string JSON response from Overpass.
  */
-function get(?int $nis5 = null): string
+function get(): string
 {
-    if (is_null($nis5)) {
-        $query = file_get_contents('scripts/overpass/associatedStreet-full-json');
-    } else {
-        $query = file_get_contents('scripts/overpass/associatedStreet-municipality-csv');
-        $query = str_replace('#####', $nis5, $query);
-    }
+    $query = file_get_contents('scripts/overpass/associatedStreet-full-json');
     $query = str_replace(["\r", "\n"], '', $query);
 
     $client = new \GuzzleHttp\Client();
--- scripts/street-listing/overpass/associatedStreet.php
+++ scripts/street-listing/overpass/associatedStreet.php
@@ -12,28 +12,37 @@ require 'vendor/autoload.php';
 
 $municipalities = include 'scripts/municipalities.php';
 
-$directory = 'data/overpass/highway';
+$directory = 'data/overpass/associatedStreet';
 
 if (!file_exists($directory) || !is_dir($directory)) {
     mkdir($directory);
 }
 
-// Get all the associatedStreet relations in Brussels Region.
-file_put_contents(
-    sprintf('%s/full.json', $directory),
-    get()
-);
+// Get all the associatedStreet relations in all the municipalities in Brussels Region.
+foreach ($municipalities as $nis5 => $municipality) {
+    printf('%d - %s%s', $nis5, $municipality[0], PHP_EOL);
+
+    file_put_contents(
+        sprintf('%s/%s.csv', $directory, strtolower($municipality[0])),
+        get($nis5)
+    );
+
+    sleep(mt_rand(30, 120));
+}
 
 exit(0);
 
 /**
  * Run Overpass API.
  *
- * @return string JSON response from Overpass.
+ * @param int $nis5 NIS code of the municipality.
+ *
+ * @return string CSV response from Overpass.
  */
-function get(): string
+function get(int $nis5): string
 {
-    $query = file_get_contents('scripts/overpass/highway-full-json');
+    $query = file_get_contents('scripts/overpass/associatedStreet-municipality-csv');
+    $query = str_replace('#####', $nis5, $query);
     $query = str_replace(["\r", "\n"], '', $query);
 
     $client = new \GuzzleHttp\Client();

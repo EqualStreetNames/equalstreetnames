529be667f13494cbc8e4e38e6284cc82e757051d
Jonathan Beliën <dev@jbelien.be>
2020-03-23 08:53:37 +0100

    Update statistics.php
    Improve CSV export + Remove JSON gender export.

--- scripts/statistics.php
+++ scripts/statistics.php
@@ -2,71 +2,150 @@
 
 declare(strict_types=1);
 
-chdir(__DIR__.'/../');
+chdir(__DIR__ . '/../');
 
 require 'vendor/autoload.php';
 
 $relations = json_decode(file_get_contents('static/relations.geojson'), true);
 $ways = json_decode(file_get_contents('static/ways.geojson'), true);
 
-$statistics = [
-    'f' => [],
-    'm' => [],
-    'x' => [],
-    '-' => [],
+$streets = [];
+$count = [
+    'F' => 0,
+    'M' => 0,
+    'X' => 0,
+    '-' => 0,
 ];
 
-// JSON file
+function wd_remove_accents($str, $charset = 'utf-8')
+{
+    $str = htmlentities($str, ENT_NOQUOTES, $charset);
+
+    $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
+    $str = preg_replace('#&[^;]+;#', '', $str);
+
+    return $str;
+}
+
+function already(string $name_fr, string $name_nl, ?string $gender, array $streets)
+{
+    $filter = array_filter(
+        $streets,
+        function (array $street) use ($name_fr, $name_nl, $gender) {
+            return wd_remove_accents($street['name_fr']) == wd_remove_accents($name_fr)
+                && wd_remove_accents($street['name_nl']) == wd_remove_accents($name_nl)
+                && $street['gender'] == $gender;
+        }
+    );
+
+    return count($filter) > 0 ? current($filter) : false;
+}
 
 foreach ($relations['features'] as $feature) {
     $person = isset($feature['properties']['details']) ? $feature['properties']['details']['person'] : false;
 
-    $gender = strtolower($person === true ? $feature['properties']['details']['gender'] ?? '-' : '-');
-    $name = $feature['properties']['name'];
+    $data = [
+        'name_fr'  => $feature['properties']['name:fr'] ?? $feature['properties']['name'],
+        'name_nl'  => $feature['properties']['name:nl'] ?? $feature['properties']['name'],
+        'gender'   => $person === true ? $feature['properties']['details']['gender'] ?? null : null,
+        'wikidata' => $feature['properties']['etymology'],
+        'type'     => 'relation',
+    ];
 
-    if (!in_array($name, $statistics[$gender])) {
-        $statistics[$gender][] = $name;
+    if (($street = already($data['name_fr'], $data['name_nl'], $data['gender'], $streets)) === false) {
+        $streets[] = $data;
+
+        $count[is_null($data['gender']) ? '-' : $data['gender']]++;
+    } elseif (!is_null($street['wikidata']) && !is_null($data['wikidata']) && $street['wikidata'] !== $data['wikidata']) {
+        printf('Wikidata mismatch: %s - %s : %s <> %s%s', $data['name_fr'], $data['name_nl'], $data['wikidata'], $street['wikidata'], PHP_EOL);
     }
 }
 
 foreach ($ways['features'] as $feature) {
     $person = isset($feature['properties']['details']) ? $feature['properties']['details']['person'] : false;
 
-    $gender = strtolower($person === true ? $feature['properties']['details']['gender'] ?? '-' : '-');
-    $name = $feature['properties']['name'];
+    $data = [
+        'name_fr'  => $feature['properties']['name:fr'] ?? $feature['properties']['name'],
+        'name_nl'  => $feature['properties']['name:nl'] ?? $feature['properties']['name'],
+        'gender'   => $person === true ? $feature['properties']['details']['gender'] ?? null : null,
+        'wikidata' => $feature['properties']['etymology'],
+        'type'     => 'way',
+    ];
 
-    if (!in_array($name, $statistics[$gender])) {
-        $statistics[$gender][] = $name;
+    if (($street = already($data['name_fr'], $data['name_nl'], $data['gender'], $streets)) === false) {
+        $streets[] = $data;
+
+        $count[is_null($data['gender']) ? '-' : $data['gender']]++;
+    } elseif (!is_null($street['wikidata']) && !is_null($data['wikidata']) && $street['wikidata'] !== $data['wikidata']) {
+        printf('Wikidata mismatch: %s - %s : %s <> %s%s', $data['name_fr'], $data['name_nl'], $data['wikidata'], $street['wikidata'], PHP_EOL);
     }
 }
 
-file_put_contents('static/gender.json', json_encode($statistics));
+$wikidata = array_map(
+    function ($street) {
+        return is_null($street['wikidata']);
+    },
+    $streets
+);
+$name_fr = array_map(
+    function ($street) {
+        return wd_remove_accents($street['name_fr']);
+    },
+    $streets
+);
+$name_nl = array_map(
+    function ($street) {
+        return wd_remove_accents($street['name_nl']);
+    },
+    $streets
+);
+
+array_multisort(
+    $wikidata,
+    SORT_ASC,
+    array_column($streets, 'gender'),
+    SORT_ASC,
+    $name_fr,
+    SORT_ASC,
+    $name_nl,
+    SORT_ASC,
+    $streets
+);
 
 // CSV file
 
+$previous = null;
 $fp = fopen('static/gender.csv', 'w');
+fputcsv($fp, ['name_fr', 'name_nl', 'gender', 'wikidata', 'type']);
+foreach ($streets as $street) {
+    if (in_array($street['gender'], ['F', 'M', 'X'])) {
+        if (!is_null($previous) && ($previous['name_fr'] == $street['name_fr'] || $previous['name_nl'] == $street['name_nl'])) {
+            printf('Duplicate: %s - %s <> %s - %s%s', $previous['name_fr'], $previous['name_nl'], $street['name_fr'], $street['name_nl'], PHP_EOL);
+        }
 
-fputcsv(
-    $fp,
-    ['streetname', 'gender']
-);
-
-foreach (['f', 'm', 'x'] as $gender) {
-    foreach ($statistics[$gender] as $streetname) {
-        fputcsv(
-            $fp,
-            [$streetname, strtoupper($gender)]
-        );
+        fputcsv($fp, $street);
     }
-}
 
+    $previous = $street;
+}
 fclose($fp);
 
-$total = count($statistics['f']) + count($statistics['m']) + count($statistics['x'])/* + count($statistics['-'])*/;
+// JSON file
 
-printf('Female: %d (%.2f %%)%s', count($statistics['f']), count($statistics['f']) / $total * 100, PHP_EOL);
-printf('Male: %d (%.2f %%)%s', count($statistics['m']), count($statistics['m']) / $total * 100, PHP_EOL);
-printf('Other: %d (%.2f %%)%s', count($statistics['x']), count($statistics['x']) / $total * 100, PHP_EOL);
-printf('No person: %d%s', count($statistics['-']), PHP_EOL);
+file_put_contents('static/statistics.json', json_encode($count));
+
+print(PHP_EOL);
+
+$total = $count['F'] + $count['M'] + $count['X'];
+
+printf('Person: %d%s', $total, PHP_EOL);
+printf('Female: %d (%.2f %%)%s', $count['F'], $count['F'] / $total * 100, PHP_EOL);
+printf('Male: %d (%.2f %%)%s', $count['M'], $count['M'] / $total * 100, PHP_EOL);
+printf('Other: %d (%.2f %%)%s', $count['X'], $count['X'] / $total * 100, PHP_EOL);
+
+print(PHP_EOL);
+
+printf('Not a person: %d%s', $count['-'], PHP_EOL);
 
 exit(0);

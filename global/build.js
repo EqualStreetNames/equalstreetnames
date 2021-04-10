5040fbed9f47e9a7abaf969ed01d26dbf5c95ed6
Jonathan Beliën <dev@jbelien.be>
2021-04-10 16:02:08 +0200

    Upragde global website

---
+++ global/build.js
@@ -0,0 +1,116 @@
+const fs = require("fs");
+const path = require("path");
+const shell = require("shelljs");
+const program = require("commander");
+const turf = require("@turf/turf");
+
+const version = require("./package.json").version;
+
+program.version(version);
+program.option("-s, --serve").action(bundle);
+program.parse(process.argv);
+
+async function bundle(options) {
+  const serve = options.serve || false;
+
+  try {
+    const polygons = { type: "FeatureCollection", features: [] };
+    const points = { type: "FeatureCollection", features: [] };
+
+    const citiesDetails = JSON.parse(
+      fs.readFileSync(path.resolve("./cities.json"))
+    );
+
+    const directory = path.resolve("../cities/");
+
+    const countries = fs
+      .readdirSync(directory, { withFileTypes: true })
+      .filter((directory) => directory.isDirectory());
+
+    countries.forEach((country) => {
+      const cities = fs
+        .readdirSync(path.join(directory, country.name), {
+          withFileTypes: true,
+        })
+        .filter((directory) => directory.isDirectory());
+
+      cities.forEach((city) => {
+        if (
+          typeof citiesDetails[country.name] === "undefined" ||
+          typeof citiesDetails[country.name][city.name] === "undefined"
+        ) {
+          throw new Error(
+            `Details are missing for "${country.name}/${city.name}".`
+          );
+        }
+
+        const feature = {
+          type: "Feature",
+          id: `${country.name}:${city.name}`,
+          properties: citiesDetails[country.name][city.name],
+          geometry: null,
+        };
+
+        const statistics = fs.readFileSync(
+          path.join(
+            directory,
+            country.name,
+            city.name,
+            "data",
+            "statistics.json"
+          ),
+          "utf8"
+        );
+        feature.properties.statistics = JSON.parse(statistics);
+
+        const boundary = fs.readFileSync(
+          path.join(
+            directory,
+            country.name,
+            city.name,
+            "data",
+            "boundary.geojson"
+          ),
+          "utf8"
+        );
+        feature.geometry = JSON.parse(boundary);
+
+        polygons.features.push(feature);
+
+        const centroid = turf.centerOfMass(feature.geometry);
+
+        const featurePoint = { ...feature };
+        featurePoint.geometry = centroid.geometry;
+
+        points.features.push(featurePoint);
+
+        shell.echo("✔", feature.properties.name);
+      });
+    });
+
+    shell.rm("-rf", "dist/");
+    shell.mkdir("dist/");
+
+    fs.writeFileSync(
+      path.resolve("./dist/cities.json"),
+      JSON.stringify(polygons),
+      "utf8"
+    );
+    fs.writeFileSync(
+      path.resolve("./dist/cities-point.json"),
+      JSON.stringify(points),
+      "utf8"
+    );
+
+    shell.echo("Total:", polygons.features.length, "polygons", "&", points.features.length, "points");
+
+    if (serve === true) {
+      shell.exec("npm run parcel:serve", { async: true });
+    } else {
+      shell.exec("npm run parcel:build");
+    }
+  } catch (error) {
+    shell.echo(error.message);
+    shell.exit(1);
+  }
+}
---
+++ global/src/index.ts
@@ -0,0 +1,102 @@
+'use strict';
+
+import { Map, Overlay, View } from 'ol';
+import GeoJSON from 'ol/format/GeoJSON';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import { Fill, Stroke, Circle, Style } from 'ol/style';
+
+import * as polygons from '../dist/cities.json';
+import * as points from '../dist/cities-point.json';
+import { toStringHDMS } from "ol/coordinate";
+import { toLonLat } from "ol/proj";
+
+const baselayer = new TileLayer({
+  source: new OSM(),
+});
+
+const map = new Map({
+  layers: [baselayer],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+const layerPolygons = new VectorLayer({
+  source: new VectorSource({
+    features: new GeoJSON({ featureProjection: map.getView().getProjection() }).readFeatures(polygons)
+  }),
+  minZoom: 8,
+  style: new Style({
+    fill: new Fill({
+      color: 'rgba(255,255,255,0.4)'
+    }),
+    stroke: new Stroke({
+      color: '#3399CC',
+      width: 2
+    })
+  })
+});
+map.addLayer(layerPolygons);
+
+const layerPoints = new VectorLayer({
+  source: new VectorSource({
+    features: new GeoJSON({ featureProjection: map.getView().getProjection() }).readFeatures(points)
+  }),
+  maxZoom: 8,
+  style: new Style({
+    image: new Circle({
+      fill: new Fill({
+        color: '#3399CC'
+      }),
+      stroke: new Stroke({
+        color: '#FFFFFF',
+        width: 3
+      }),
+      radius: 8
+    })
+  })
+});
+map.addLayer(layerPoints);
+
+map.getView().fit(layerPolygons.getSource().getExtent(), { padding: [100, 100, 100, 100] });
+
+const overlay = new Overlay({
+  element: document.getElementById('popup'),
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+map.addOverlay(overlay);
+
+map.on('singleclick', (event) => {
+  const features = map.getFeaturesAtPixel(event.pixel);
+
+  if (features.length > 0) {
+    const { name, url, statistics } = features[0].getProperties();
+
+    const total = Object.values(statistics).reduce((a, b) => a + b, 0);
+    const totalPerson = total - statistics['-'];
+
+    let html = `${name}<br><a target="_blank" href="${url}">${url}</a>`;
+    html += '<hr>';
+    html += `<div style="font-size: small;">`
+      + `Out of ${total} streetnames,<br>${totalPerson} have been found to be named after a person :<br>`
+      + (statistics.F > 0 ? `${statistics.F} after a cisgender female,<br>` : '')
+      + (statistics.FX > 0 ? `${statistics.FX} after a transgender female,<br>` : '')
+      + (statistics.MX > 0 ? `${statistics.MX} after a transgender male,<br>` : '')
+      + (statistics.M > 0 ? `${statistics.M} after a cisgender male,<br>` : '')
+      + `</div>`;
+
+    overlay.getElement().innerHTML = html;
+
+    overlay.setPosition(event.coordinate);
+  } else {
+    overlay.setPosition(undefined);
+  }
+});

dfe0820edd18ca8436367c44f2292f2880792530
Jonathan Beliën <dev@jbelien.be>
2021-04-10 17:12:22 +0200

    Update global website
    Add overlay.

--- global/src/index.ts
+++ global/src/index.ts
@@ -10,93 +10,98 @@ import { Fill, Stroke, Circle, Style } from 'ol/style';
 
 import * as polygons from '../dist/cities.json';
 import * as points from '../dist/cities-point.json';
-import { toStringHDMS } from "ol/coordinate";
-import { toLonLat } from "ol/proj";
 
-const baselayer = new TileLayer({
-  source: new OSM(),
-});
 
-const map = new Map({
-  layers: [baselayer],
-  target: 'map',
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+(function () {
+  document.getElementById('count').innerText = polygons.features.length.toString();
 
-const layerPolygons = new VectorLayer({
-  source: new VectorSource({
-    features: new GeoJSON({ featureProjection: map.getView().getProjection() }).readFeatures(polygons)
-  }),
-  minZoom: 8,
-  style: new Style({
-    fill: new Fill({
-      color: 'rgba(255,255,255,0.4)'
+  const baselayer = new TileLayer({
+    source: new OSM(),
+  });
+
+  const map = new Map({
+    layers: [baselayer],
+    target: 'map',
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
     }),
-    stroke: new Stroke({
-      color: '#3399CC',
-      width: 2
-    })
-  })
-});
-map.addLayer(layerPolygons);
+  });
 
-const layerPoints = new VectorLayer({
-  source: new VectorSource({
-    features: new GeoJSON({ featureProjection: map.getView().getProjection() }).readFeatures(points)
-  }),
-  maxZoom: 8,
-  style: new Style({
-    image: new Circle({
+  const layerPolygons = new VectorLayer({
+    source: new VectorSource({
+      features: new GeoJSON({ featureProjection: map.getView().getProjection() }).readFeatures(polygons)
+    }),
+    minZoom: 8,
+    style: new Style({
       fill: new Fill({
-        color: '#3399CC'
+        color: 'rgba(255,255,255,0.4)'
       }),
       stroke: new Stroke({
-        color: '#FFFFFF',
-        width: 3
-      }),
-      radius: 8
+        color: '#3399CC',
+        width: 2
+      })
     })
-  })
-});
-map.addLayer(layerPoints);
+  });
+  map.addLayer(layerPolygons);
 
-map.getView().fit(layerPolygons.getSource().getExtent(), { padding: [100, 100, 100, 100] });
+  const layerPoints = new VectorLayer({
+    source: new VectorSource({
+      features: new GeoJSON({ featureProjection: map.getView().getProjection() }).readFeatures(points)
+    }),
+    maxZoom: 8,
+    style: new Style({
+      image: new Circle({
+        fill: new Fill({
+          color: '#3399CC'
+        }),
+        stroke: new Stroke({
+          color: '#FFFFFF',
+          width: 3
+        }),
+        radius: 8
+      })
+    })
+  });
+  map.addLayer(layerPoints);
 
-const overlay = new Overlay({
-  element: document.getElementById('popup'),
-  autoPan: true,
-  autoPanAnimation: {
-    duration: 250,
-  },
-});
-map.addOverlay(overlay);
+  map.getView().fit(layerPolygons.getSource().getExtent(), { padding: [100, 100, 100, 100] });
 
-map.on('singleclick', (event) => {
-  const features = map.getFeaturesAtPixel(event.pixel);
+  const overlay = new Overlay({
+    element: document.getElementById('popup'),
+    autoPan: true,
+    autoPanAnimation: {
+      duration: 250,
+    },
+  });
+  map.addOverlay(overlay);
 
-  if (features.length > 0) {
-    const { name, url, statistics } = features[0].getProperties();
+  map.on('singleclick', (event) => {
+    const features = map.getFeaturesAtPixel(event.pixel);
 
-    const total = Object.values(statistics).reduce((a, b) => a + b, 0);
-    const totalPerson = total - statistics['-'];
+    if (features.length > 0) {
+      const { name, url, statistics } = features[0].getProperties();
 
-    let html = `${name}<br><a target="_blank" href="${url}">${url}</a>`;
-    html += '<hr>';
-    html += `<div style="font-size: small;">`
-      + `Out of ${total} streetnames,<br>${totalPerson} have been found to be named after a person :<br>`
-      + (statistics.F > 0 ? `${statistics.F} after a cisgender female,<br>` : '')
-      + (statistics.FX > 0 ? `${statistics.FX} after a transgender female,<br>` : '')
-      + (statistics.MX > 0 ? `${statistics.MX} after a transgender male,<br>` : '')
-      + (statistics.M > 0 ? `${statistics.M} after a cisgender male,<br>` : '')
-      + `</div>`;
+      const total = Object.values(statistics).reduce((a, b) => a + b, 0);
+      const totalPerson = total - statistics['-'];
 
-    overlay.getElement().innerHTML = html;
+      let html = `${name}<br><a target="_blank" href="${url}">${url}</a>`;
+      html += '<hr>';
+      html += `<div style="font-size: small;">`
+        + `Out of ${total} streetnames,<br>${totalPerson} have been found to be named after a person :<br>`
+        + (statistics.F > 0 ? `${statistics.F} after a cisgender female,<br>` : '')
+        + (statistics.FX > 0 ? `${statistics.FX} after a transgender female,<br>` : '')
+        + (statistics.MX > 0 ? `${statistics.MX} after a transgender male,<br>` : '')
+        + (statistics.M > 0 ? `${statistics.M} after a cisgender male,<br>` : '')
+        + `</div>`;
 
-    overlay.setPosition(event.coordinate);
-  } else {
-    overlay.setPosition(undefined);
-  }
-});
+      overlay.getElement().innerHTML = html;
+
+      overlay.setPosition(event.coordinate);
+    } else {
+      overlay.setPosition(undefined);
+    }
+  });
+
+  map.updateSize();
+})();

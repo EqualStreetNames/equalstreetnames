c8157ddf334adb1d6a7b57988e00a6a4f9b64e66
Jonathan Beliën <dev@jbelien.be>
2021-04-17 19:42:47 +0200

    Update process.php

--- process/process.php
+++ process/process.php
@@ -4,10 +4,12 @@
 require __DIR__.'/vendor/autoload.php';
 
 use App\Command\OverpassCommand;
+use App\Command\WikidataCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application('EqualStreetNames Data process');
 
 $application->add(new OverpassCommand());
+$application->add(new WikidataCommand());
 
 $application->run();

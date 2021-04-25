653824b6303c7a3353f343af04afed43a5a54d13
Jonathan Beliën <dev@jbelien.be>
2021-04-25 16:10:04 +0200

    🚨 Apply PHPCS fixes

--- process/Wikidata/Wikidata.php
+++ process/Wikidata/Wikidata.php
@@ -4,134 +4,136 @@ namespace App\Wikidata;
 
 class Wikidata
 {
-  public static function extractLabels($entity, array $languages): array
-  {
-    $labels = [];
+    public static function extractLabels($entity, array $languages): array
+    {
+        $labels = [];
 
-    foreach ($languages as $language) {
-      if (isset($entity->labels->{$language})) {
-        $labels[$language] = $entity->labels->{$language};
-      }
+        foreach ($languages as $language) {
+            if (isset($entity->labels->{$language})) {
+                $labels[$language] = $entity->labels->{$language};
+            }
+        }
+
+        return $labels;
     }
 
-    return $labels;
-  }
+    public static function extractDescriptions($entity, array $languages): array
+    {
+        $descriptions = [];
 
-  public static function extractDescriptions($entity, array $languages): array
-  {
-    $descriptions = [];
+        foreach ($languages as $language) {
+            if (isset($entity->descriptions->{$language})) {
+                $descriptions[$language] = $entity->descriptions->{$language};
+            }
+        }
 
-    foreach ($languages as $language) {
-      if (isset($entity->descriptions->{$language})) {
-        $descriptions[$language] = $entity->descriptions->{$language};
-      }
+        return $descriptions;
     }
 
-    return $descriptions;
-  }
+    public static function extractSitelinks($entity, array $languages): array
+    {
+        $sitelinks = [];
 
-  public static function extractSitelinks($entity, array $languages): array
-  {
-    $sitelinks = [];
+        foreach ($languages as $language) {
+            if (isset($entity->sitelinks->{$language . 'wiki'})) {
+                $sitelinks[$language . 'wiki'] = $entity->sitelinks->{$language . 'wiki'};
+            }
+        }
 
-    foreach ($languages as $language) {
-      if (isset($entity->sitelinks->{$language.'wiki'})) {
-        $sitelinks[$language.'wiki'] = $entity->sitelinks->{$language.'wiki'};
-      }
+        return $sitelinks;
     }
 
-    return $sitelinks;
-  }
+    public static function extractNicknames($entity, array $languages): ?array
+    {
+        $nicknames = null;
 
-  public static function extractNicknames($entity, array $languages): ?array
-  {
-    $nicknames = null;
+        $claims = $entity->claims->P1449 ?? [];
 
-    $claims = $entity->claims->P1449 ?? [];
+        foreach ($claims as $value) {
+            $language = $value->mainsnak->datavalue->value->language;
 
-    foreach ($claims as $value) {
-      $language = $value->mainsnak->datavalue->value->language;
+            if (in_array($language, $languages, true)) {
+                $nicknames[$language] = $value->mainsnak->datavalue->value;
+            }
+        }
 
-      if (in_array($language, $languages, true)) {
-        $nicknames[$language] = $value->mainsnak->datavalue->value;
-      }
+        return $nicknames;
     }
 
-    return $nicknames;
-  }
-
-  public static function extractDateOfBirth($entity): ?string
-  {
-    return $entity->claims->P569[0]->mainsnak->datavalue->value->time ?? null;
-  }
-
-  public static function extractDateOfDeath($entity): ?string
-  {
-    return $entity->claims->P570[0]->mainsnak->datavalue->value->time ?? null;
-  }
-
-  public static function extractImage($entity): ?string
-  {
-    return $entity->claims->P18[0]->mainsnak->datavalue->value ?? null;
-  }
-
-  public static function extractGender($entity): ?string
-  {
-    $identifier = $entity->claims->P21[0]->mainsnak->datavalue->value->id ?? null;
-
-    switch ($identifier) {
-      case 'Q6581097': // male
-      case 'Q15145778': // male (cis)
-        return 'M';
-
-      case 'Q6581072': // female
-      case 'Q15145779': // female (cis)
-        return 'F';
-
-      case 'Q1052281': // female (trans)
-        return 'FX';
-
-      case 'Q2449503': // male (trans)
-        return 'MX';
-
-      case 'Q1097630': // intersex
-        return 'X';
-
-      case 'Q48270': // non-binary
-        return 'NB';
-
-      default:
-        return null;
-    }
-  }
-
-  private static function extractInstances($entity): ?array
-  {
-    $property = $entity->claims->P31 ?? $entity->claims->P279 ?? null;
-
-    if (is_null($property)) {
-      return null;
+    public static function extractDateOfBirth($entity): ?string
+    {
+        return $entity->claims->P569[0]->mainsnak->datavalue->value->time ?? null;
     }
 
-    return array_map(function ($p) { return $p->mainsnak->datavalue->value->id; }, $property);
-  }
-
-  public static function isPerson($entity, array $instances): ?bool
-  {
-    $identifiers = self::extractInstances($entity);
-
-    if (is_null($identifiers)) {
-      return null;
+    public static function extractDateOfDeath($entity): ?string
+    {
+        return $entity->claims->P570[0]->mainsnak->datavalue->value->time ?? null;
     }
 
-    $person = false;
-    foreach ($identifiers as $id) {
-      if (isset($instances[$id]) && $instances[$id] === true) {
-        $person = true;
-        break;
-      }
+    public static function extractImage($entity): ?string
+    {
+        return $entity->claims->P18[0]->mainsnak->datavalue->value ?? null;
     }
 
-    return $person;
-  }
+    public static function extractGender($entity): ?string
+    {
+        $identifier = $entity->claims->P21[0]->mainsnak->datavalue->value->id ?? null;
+
+        switch ($identifier) {
+            case 'Q6581097': // male
+            case 'Q15145778': // male (cis)
+                return 'M';
+
+            case 'Q6581072': // female
+            case 'Q15145779': // female (cis)
+                return 'F';
+
+            case 'Q1052281': // female (trans)
+                return 'FX';
+
+            case 'Q2449503': // male (trans)
+                return 'MX';
+
+            case 'Q1097630': // intersex
+                return 'X';
+
+            case 'Q48270': // non-binary
+                return 'NB';
+
+            default:
+                return null;
+        }
+    }
+
+    private static function extractInstances($entity): ?array
+    {
+        $property = $entity->claims->P31 ?? $entity->claims->P279 ?? null;
+
+        if (is_null($property)) {
+            return null;
+        }
+
+        return array_map(function ($p) {
+            return $p->mainsnak->datavalue->value->id;
+        }, $property);
+    }
+
+    public static function isPerson($entity, array $instances): ?bool
+    {
+        $identifiers = self::extractInstances($entity);
+
+        if (is_null($identifiers)) {
+            return null;
+        }
+
+        $person = false;
+        foreach ($identifiers as $id) {
+            if (isset($instances[$id]) && $instances[$id] === true) {
+                $person = true;
+                break;
+            }
+        }
+
+        return $person;
+    }
 }

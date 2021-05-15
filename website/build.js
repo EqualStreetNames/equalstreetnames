038731b411025ac4c61d0a992be80eaeea2d5601
1-Byte <1-Byte@users.noreply.github.com>
2021-05-15 12:18:29 +0200

    Report errors during build process (#197)
    * Report errors during build process
    
    * Update build.js
    
    Use `shell` instead of `process`.
    
    * Apply fix to global build
    
    Co-authored-by: Jonathan Beliën <dev@jbelien.be>

--- website/build.js
+++ website/build.js
@@ -14,7 +14,12 @@ program.version(version);
 
 program.option('-c, --city <path>').option('-s, --serve').action(bundle);
 
-program.parse(process.argv);
+program
+  .parseAsync(process.argv)
+  .catch(error => {
+    shell.echo(`Error: ${error}`);
+    shell.exit(1);
+  });
 
 async function bundle (options) {
   const city = options.city;

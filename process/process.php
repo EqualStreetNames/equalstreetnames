44ac8e83bb36865aaf477add6d3fd53b814fae3f
Jonathan Beliën <dev@jbelien.be>
2021-06-05 11:33:06 +0200

    Update process.php

--- process/process.php
+++ process/process.php
@@ -7,6 +7,7 @@ use App\Command\BoundaryCommand;
 use App\Command\GeoJSONCommand;
 use App\Command\OverpassCommand;
 use App\Command\StatisticsCommand;
+use App\Command\Tool\CalendarCommand;
 use App\Command\Tool\NormalizeCSVCommand;
 use App\Command\WikidataCommand;
 use Symfony\Component\Console\Application;
@@ -19,6 +20,7 @@ $application->add(new BoundaryCommand());
 $application->add(new GeoJSONCommand());
 $application->add(new StatisticsCommand());
 
+$application->add(new CalendarCommand());
 $application->add(new NormalizeCSVCommand());
 
 $application->run();

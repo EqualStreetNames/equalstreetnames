2359f1bf23669e616857b536077015150337cefb
Jonathan Beliën <dev@jbelien.be>
2021-06-12 10:57:13 +0200

    Update index.ts
    Update init + Use `data-` instead of globals.

--- website/src/script/index.ts
+++ website/src/script/index.ts
@@ -17,13 +17,26 @@ export let bbox: [number, number, number, number];
 export let countries: string;
 export let style: string;
 
-export function init () {
+(function () {
   const html = document.querySelector('html') as HTMLHtmlElement;
   lang = html.getAttribute('lang') ?? 'en';
+
+  const data = document.getElementById('map')?.dataset;
+
+  countries = data?.countries ?? '';
+  style = data?.style ?? '';
+
+  if (typeof data?.center !== 'undefined' && typeof data?.zoom !== 'undefined') {
+    center = JSON.parse(data.center);
+    zoom = parseFloat(data.zoom);
+  }
+  if (typeof data?.bbox !== 'undefined') {
+    bbox = JSON.parse(data.bbox);
+  }
 
   initMap();
 
   initChart(
     document.querySelector('#gender-chart > canvas') as HTMLCanvasElement
   );
-}
+})();

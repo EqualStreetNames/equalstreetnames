b40ca8110d2599bde671ab910471361c062f3433
Jonathan Beliën <dev@jbelien.be>
2022-06-08 21:32:37 +0200

    Move website to submodule

--- website/src/script/wikidata/death.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default function (person: { death?: number }): number | null {
-  if (typeof person.death === 'undefined') {
-    return null;
-  }
-
-  return person.death;
-}
--- website/src/script/wikidata/gender.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default function (person: { gender?: string }): string | null {
-  if (typeof person.gender === 'undefined') {
-    return null;
-  }
-
-  return person.gender;
-}
